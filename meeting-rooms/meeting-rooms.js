16ec972a54378b0899969bcd3605ac5cc13118b1
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-09-11 23:19:17 -0400

    Time: 112 ms (21.90%), Space: 40.1 MB (96.76%) - LeetHub

---
+++ meeting-rooms/meeting-rooms.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+    intervals.sort(function(a,b) {
+        return a[0] - b[0];
+    });
+    
+   for(let i=0;i<intervals.length-1;i++)
+       {
+           if(intervals[i][1] > intervals[i+1][0])
+                   return false;
+       }
+    return true;
+};
