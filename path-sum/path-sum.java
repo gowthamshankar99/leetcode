589210c51718d3e81a308086af847194cc945f0b
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-09-20 20:28:55 -0400

    Time: 1 ms (26.46%), Space: 40.9 MB (6.87%) - LeetHub

--- path-sum/path-sum.java
+++ path-sum/path-sum.java
@@ -15,23 +15,52 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        
         if(root == null) {
             return false;
         }
         
         targetSum = targetSum - root.val;
         
-        if(root.left == null && root.right == null) {
+        if(root.left == null && root.right == null)
+        {
             if(targetSum == 0) {
                 return true;
+            } else {
+                return false;
             }
-         }
-    
-        // iterate through the left subtree
-    
+        }    
+        
         return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
-        
+    }
     
+    public int hasPathSum2(TreeNode root, int targetSum) {
+        if(root == null) {
+            System.out.println(targetSum);
+            return targetSum;
+        }  
         
+        if(root.left == null && root.right == null)
+            return targetSum;
+        
+        if(root.left != null || root.right != null) {
+             if(root.left == null) {
+                 targetSum = targetSum + Math.max(0, root.right.val);
+                 return hasPathSum2(root.right, targetSum);
+                 
+             } 
+            if(root.right == null) {
+                targetSum = targetSum + Math.max(0, root.left.val);
+                return hasPathSum2(root.left, targetSum);
+            }
+            
+            targetSum = targetSum + Math.max(root.right.val, root.left.val);
+            if(Math.max(root.right.val, root.left.val) == root.left.val)
+                return hasPathSum2(root.left, targetSum);
+            else 
+                return hasPathSum2(root.right, targetSum);
+            
+        }
+        return 0;
     }
 }
