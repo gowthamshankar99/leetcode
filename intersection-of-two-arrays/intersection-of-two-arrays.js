d3d9dc7dc10d70a4b206f4f2f92f61d259ab1527
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-09-26 22:09:20 -0400

    Time: 80 ms (60.88%), Space: 39.3 MB (94.42%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.js
+++ intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -7,7 +7,6 @@ var intersection = function(nums1, nums2) {
     let result = [];
     for(let i=0;i<nums2.length;i++) {
         if(nums1.includes(nums2[i]) && !result.includes(nums2[i])) {
-            
             result.push(nums2[i]);
         } 
     }
