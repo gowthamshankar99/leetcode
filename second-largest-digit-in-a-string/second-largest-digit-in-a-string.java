97b873824f2163a0c77649f58aa9d354d999ca38
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-10-04 02:13:14 -0400

    Time: 9 ms (15.20%), Space: 38.8 MB (63.89%) - LeetHub

--- second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
+++ second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
@@ -6,17 +6,17 @@ class Solution {
 
         for(int i=0;i<s.length();i++) {
             if(Character.isDigit(s.charAt(i))) {
-                 if(!queue.contains(Character.getNumericValue(s.charAt(i))))
-                    queue.add(Character.getNumericValue(s.charAt(i)));
+                 int charToAdd = Character.getNumericValue(s.charAt(i));
+                 if(!queue.contains(charToAdd))
+                    queue.add(charToAdd);
                  if(queue.size() > 2) {
                      queue.remove();
                  }
             }
         }
         
-        if(queue.isEmpty() || queue.size() < 2 )
+        if(queue.isEmpty() || queue.size() < 2)
             return -1;
-    
         
         int secondLargestElement = queue.remove();
         
