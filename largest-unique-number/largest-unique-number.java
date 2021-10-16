44a0cdc453fff2a850f407dd02bd97d5caad1c20
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-10-16 18:53:35 -0400

    Time: 1 ms (92.15%), Space: 38.6 MB (60.21%) - LeetHub

---
+++ largest-unique-number/largest-unique-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestUniqueNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i=nums.length-1;i>=0;i--) {
+            if(i == 0 || nums[i] != nums[i-1])
+                return nums[i];
+            
+            // if its equal
+            while(i>0 && nums[i] == nums[i-1]) {
+                i--;
+            }
+            
+        }
+        
+        return -1;
+    }
+}
