b24b4f3afcd490460cd131f794f09f537b6bb9f8
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-10-16 18:30:00 -0400

    Time: 84 ms (67.06%), Space: 40.6 MB (34.84%) - LeetHub

---
+++ flipping-an-image/flipping-an-image.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    for(let i=0;i<image.length;i++) {
+            image[i].reverse();
+    }
+    
+    for(let i=0;i<image.length;i++) {
+           for(let j=0;j<image[0].length;j++) {
+               if(image[i][j] == 1)
+                   image[i][j] = 0;
+               else
+                   image[i][j] = 1;
+           }
+    }
+    
+    return image;
+};
