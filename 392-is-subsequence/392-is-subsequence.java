b35efb5c46516b8c3385d240f38dfa2455ffc67a
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-03-20 23:04:45 -0400

    Time: 1 ms (89.05%), Space: 42.5 MB (15.39%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,16 +1,20 @@
 class Solution {
 
     public boolean isSubsequence(String s, String t) {
-        Integer leftBound = s.length(), rightBound = t.length();
-        Integer pLeft = 0, pRight = 0;
-
-        while (pLeft < leftBound && pRight < rightBound) {
-            // move both pointers or just the right pointer
-            if (s.charAt(pLeft) == t.charAt(pRight)) {
-                pLeft += 1;
-            }
-            pRight += 1;
+        if(s.contentEquals("")) {
+            return true;
         }
-        return pLeft == leftBound;
+        
+        int index = 0;
+        for(int i=0;i<t.length();i++) {
+            if(s.charAt(index) == t.charAt(i)) {
+                index++;
+            }
+            
+            if(index == s.length())
+                return true;
+        }
+        
+        return false;
     }
 }
