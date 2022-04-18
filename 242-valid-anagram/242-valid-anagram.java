5aeb35dcf575078686ff01306e4036979ba5c460
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-04-17 23:22:18 -0400

    Time: 3 ms (92.05%), Space: 42 MB (96.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()) 
+            return false;
+        
          // array to hold 
         int[] resultArray = new int[26];
         
         for(int i=0;i<s.length();i++) {
             resultArray[s.charAt(i) - 'a']++;
+            resultArray[t.charAt(i) - 'a']--;
         }
-        
-        for(int j=0;j<t.length();j++) {
-            resultArray[t.charAt(j) - 'a']--;
-        }
+    
         
         //loop through the array 
         for(int val : resultArray) {
