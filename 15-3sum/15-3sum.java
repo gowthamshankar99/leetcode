6b1d01016a344f59765f793951215b7f70212d97
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-04-20 23:30:42 -0400

    Time: 2385 ms (5.01%), Space: 61.3 MB (25.04%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         // sort the Array 
-        Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
         
-        for(int i=0;i<nums.length-2;i++) {
-            if(i==0 || (i>0 && nums[i-1] != nums[i])) {
-                 int low = i+1;
-                 int high = nums.length -1;
-                 int sum = 0-nums[i];
-                 while(low < high) {
-                     if(sum == nums[low] + nums[high]) {
-                         List<Integer> innerList = new ArrayList<Integer>();
-                         innerList.add(nums[i]);
-                         innerList.add(nums[low]);
-                         innerList.add(nums[high]);
-                         list.add(innerList);
-                         while(low < high && nums[low] == nums[low+1]) low++;
-                         while(low < high && nums[high] == nums[high-1]) high--;
-                         low++;
-                         high--;
-                     } else if(nums[low] + nums[high] > sum) {
-                         high--;
-                     } else {
-                         low++;
-                     }
-                 }
+          // Result
+          List<List<Integer>> result = new ArrayList<List<Integer>>();
+          List<Integer> subList = null;
+          Arrays.sort(nums);
+          for(int i=0;i<nums.length-2;i++) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+            int start = i+1;
+            int end = nums.length-1;
+
+            while(start < end) {
+                if(nums[start] + nums[end] + nums[i] > 0) {
+                    end--;
+                } else if(nums[start] + nums[end] + nums[i] < 0) {
+                    start++;
+                } else {
+                    //System.out.println(nums[i] + " " + nums[start] + " " + nums[end]);
+                    subList = new ArrayList<Integer>();
+                    subList.add(nums[i]);
+                    subList.add(nums[start]);
+                    subList.add(nums[end]);
+                    if(!result.contains(subList)) 
+                        result.add(subList);
+                    start++;
+                    end--;
+                }
             }
-        }
         
-        return list;
+        }
+       }
+        
+        return result;
     }
 }
