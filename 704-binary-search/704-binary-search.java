aaaef5a5f4fef880bc35ab9e4dea34466bd344ce
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-04-21 23:41:02 -0400

    Time: 0 ms (100.00%), Space: 54.9 MB (16.39%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+    
+        return binarySearchNonRecursive(nums,0, nums.length-1, target);
+        
+    }
+    
+    public int binarySearch(int[] nums, int start, int end, int target) {
+        int midPoint = (start + (end))/2;
+        
+        if(nums[midPoint] > target) {
+             end = midPoint-1;
+             binarySearch(nums, start, end, target);
+        } else if(nums[midPoint] < target) {
+             start = midPoint+1;
+             binarySearch(nums, start, end, target);
+        } else {
+            return midPoint;
+        }
+        
+        return -1;
+    }
+    
+    
+    public int binarySearchNonRecursive(int[] nums, int start, int end, int target) {
+        while(start <= end) {
+            int midPoint = (start + (end))/2;
+             if(nums[midPoint] > target) {
+                 end = midPoint-1;
+            } else if(nums[midPoint] < target) {
+                 start = midPoint+1;
+            } else {
+                return midPoint;
+            }
+        }
+        
+        return -1;
+    }
+}
