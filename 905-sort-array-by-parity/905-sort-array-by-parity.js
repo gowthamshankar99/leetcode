3f826d5a5ec721da36f38f1406ea79ceb27d0c87
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-05-03 11:19:39 -0400

    Time: 144 ms (12.39%), Space: 44.4 MB (81.86%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -11,12 +11,6 @@ var sortArrayByParity = function(nums) {
              arr.push(nums[i])
          }
      }
-    
- //   for(let i=0;i<nums.length;i++) {
- //        if(nums[i]%2 != 0) {
- //            arr.push(nums[i]);
- //        } 
- //    }
 
     return arr;
 };
