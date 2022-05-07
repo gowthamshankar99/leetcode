859eaa58cd999cabe38276502a4b67ee583f7d9d
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-05-07 19:36:05 -0400

    Time: 2 ms (70.91%), Space: 41.9 MB (31.29%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -9,25 +9,8 @@ class Solution {
         if(stones.length == 1)
             return stones[0];
         
-//        Arrays.sort(stones);
-        
         Queue<Integer> queue = new PriorityQueue<Integer>((a,b) -> b-a);
-        
- //       int pointer1 = stones.length-1;
- //       int pointer2 = stones.length-2;
-        
- //       while(pointer2 >= 0) {
- //           if(stones[pointer1] != stones[pointer2])
- //               queue.add(stones[pointer1] - stones[pointer2]);
-            
- //           pointer1 = pointer1 - 2;
- //           pointer2 = pointer2 - 2;
- //       }
-        
-        
-//        if(pointer1 == 0) queue.add(stones[pointer1]);
-        
-        
+
         for(int i=0;i<stones.length;i++) {
             queue.add(stones[i]);
         }
