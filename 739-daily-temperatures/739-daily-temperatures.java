8e9498288dd4c9957f869ada796cb12a5768d14f
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-05-08 12:47:21 -0400

    Time: 28 ms (83.29%), Space: 128.2 MB (56.65%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,15 +1,20 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-          int[] result = new int[temperatures.length];
-          for(int i=0;i<temperatures.length;i++) {
-              for(int j=i+1;j<temperatures.length;j++) {
-                    if(temperatures[j] > temperatures[i]) {
-                          result[i] = j-i;
-                          break;
-                    }
-              }
-          }
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
         
-        return result;
+        for (int currDay = 0; currDay < n; currDay++) {
+            int currentTemp = temperatures[currDay];
+            // Pop until the current day's temperature is not
+            // warmer than the temperature at the top of the stack
+            while (!stack.isEmpty() && temperatures[stack.peek()] < currentTemp) {
+                int prevDay = stack.pop();
+                answer[prevDay] = currDay - prevDay;
+            }
+            stack.push(currDay);
+        }
+        
+        return answer;
     }
 }
