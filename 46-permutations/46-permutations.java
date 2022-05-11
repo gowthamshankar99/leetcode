c1f98ebb1fe067fd6e2b704f705c994b7248d688
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-05-11 11:21:54 -0400

    Time: 6 ms (7.43%), Space: 45.6 MB (5.16%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -5,13 +5,13 @@ class Solution {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         List<Integer> currentList = new ArrayList<Integer>();
         
-        backtrack2(nums, result, "", currentList);
+        backtrack2(nums, result, currentList);
         
         return result;
         
     }
     
-    public void backtrack2(int[] nums, List<List<Integer>> result, String currentString, List<Integer> currentList) {
+    public void backtrack2(int[] nums, List<List<Integer>> result, List<Integer> currentList) {
         
         if(currentList.size() == nums.length) {
             // loop through the currentString 
@@ -23,32 +23,8 @@ class Solution {
             String c= Integer.toString(nums[i]);  
             if(!currentList.contains(nums[i])) {            
                 currentList.add(nums[i]);
-                backtrack2(nums, result, currentString + nums[i], currentList);
+                backtrack2(nums, result, currentList);
                 currentList.remove(currentList.size()-1);
-            }
-        }
-    }
-    
-    
-    public void backtrack(int[] nums, List<List<Integer>> result, String currentString, List<Integer> currentList) {
-        
-        //if(currentString.length() == nums.length) {
-        if(currentString.length() == nums.length) {
-            System.out.println(currentString);
-            // loop through the currentString 
-            for(int i=0;i<currentString.length();i++) {
-                currentList.add(Character.getNumericValue(currentString.charAt(i)));
-            }
-            result.add(currentList);
-            
-            return;
-        }
-        
-        for(int i=0;i<nums.length;i++) {
-            String c= Integer.toString(nums[i]);  
-            if(!currentString.contains(c)) {            
-                currentList = new ArrayList<Integer>();
-                backtrack(nums, result, currentString + nums[i], currentList);
             }
         }
     }
