c1b3edafa1e4433cd3f0d0b53f525438485448bd
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-05-15 15:04:12 -0400

    Time: 6 ms (11.88%), Space: 44.1 MB (18.13%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -3,10 +3,10 @@ class Solution {
         List<Double> set = new ArrayList<Double>();
         
         for(int element: arr) {
-            if(set.contains((2*(double)(element))) || set.contains(((double)element/2))) {
+            if(set.contains((2.0*(element))) || set.contains((element/2.0))) {
                 return true;
             }
-            set.add((double)element);
+            set.add(element*1.0);
         }
         
         return false;
