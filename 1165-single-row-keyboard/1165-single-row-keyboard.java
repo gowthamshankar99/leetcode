d95197b6fbb552809ab09cb9c0bae09b1ea128ad
gowthamshankar99 <gowthamshankar0914@gmail.com>
2022-05-22 22:53:11 -0400

    Time: 20 ms (8.27%), Space: 43 MB (32.36%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.java
+++ 1165-single-row-keyboard/1165-single-row-keyboard.java
@@ -4,9 +4,7 @@ class Solution {
         int currentIndex = 0;
         int result = 0;
         
-        for(int i=0;i<word.length();i++) {
-            //System.out.println(keyboard.indexOf(word.charAt(i)));
-            
+        for(int i=0;i<word.length();i++) {            
             result = result + Math.abs(keyboard.indexOf(word.charAt(i)) - currentIndex);
             currentIndex = keyboard.indexOf(word.charAt(i));
         }
